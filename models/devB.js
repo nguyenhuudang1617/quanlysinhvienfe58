1c763501139aeee54a5dca2013950eb6326b553b
nguyen dang <nguyenhuudang1617@gmail.com>
2021-03-10 19:47:05 +0700

    'merge_devAB'

--- models/devB.js
+++ models/devB.js
@@ -1,7 +1,9 @@
-var devB = function() {
+var devB = function () {
     this.ma = '';
     this.hoTen = '';
     this.hienThiThongTin = function () {
         console.log('devB');
     }
 }
+//git merge [tenNhanh cần gộp code tại local]
+//git pull origin [tenNhanh cần gộp code tai server]
